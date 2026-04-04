cdf3c20f530dc56c6ea41cce574ab034e2645818
VikramS04 <vikramsaini7723@gmail.com>
2026-04-04 18:47:23 +0530

    New updates

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 import express from 'express';
 import { connectDb } from './src/db/dbConnection.js';
 import { configDotenv } from 'dotenv';
+import cors from 'cors';
+import errorHandler from './src/middlewares/errorHandler.js';
+
 
 configDotenv();
 connectDb();
+
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+
+app.use(express.json()); 
+
+app.get('/', (req,res)=>{
+  res.send("API Running...")
+});
+
+//Routes
+
+// app.use('/api/expense', );
+// app.use('/api/users', );
+app.use(errorHandler);
+
+
+app.listen(PORT,()=>{
+console.log(`Server is running on ${PORT}`);
+})
--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 export const connectDb = async () => {
   try {
     const connect = await mongoose.connect(process.env.MONGODB_URI)
